f01b52dbbb8bbf72609b828c87d84a584ec52d32
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-02-20 09:25:55 -0800

    ungroup

--- Sources/StitchViewKit/StitchNestedList/View/Buttons/DeleteSelectionsCallback.swift
+++ Sources/StitchViewKit/StitchNestedList/View/Buttons/DeleteSelectionsCallback.swift
@@ -11,15 +11,18 @@ public struct DeleteSelectionsButton<Element: StitchNestedListElement,
                               LabelView: View>: View {
     @Binding var data: [Element]
     @Binding var selections: Set<Element.ID>
+    @Binding var editMode: EditMode
     var deleteSelectionsCallback: ((Set<Element.ID>) -> ())?
     @ViewBuilder var label: () -> LabelView
     
     public init(data: Binding<[Element]>,
                 selections: Binding<Set<Element.ID>>,
+                editMode: Binding<EditMode>,
                 deleteSelectionsCallback: ((Set<Element.ID>) -> ())? = nil,
                 label: @escaping () -> LabelView) {
         self._data = data
         self._selections = selections
+        self._editMode = editMode
         self.deleteSelectionsCallback = deleteSelectionsCallback
         self.label = label
     }
@@ -31,8 +34,12 @@ public struct DeleteSelectionsButton<Element: StitchNestedListElement,
             selections.forEach {
                 data.remove($0)
             }
+            
+            self.selections = .init()
+            self.editMode = .inactive
         } label: {
             label()
         }
+        .disabled(selections.isEmpty)
     }
 }
---
+++ Sources/StitchViewKit/StitchNestedList/View/Buttons/UngroupButton.swift
@@ -0,0 +1,59 @@
+//
+//  UngroupButton.swift
+//
+//
+//  Created by Elliot Boschwitz on 2/19/24.
+//
+
+import Foundation
+import SwiftUI
+
+public struct UngroupButton<Element: StitchNestedListElement,
+                            LabelView: View>: View {
+    @Binding var data: [Element]
+    @Binding var selections: Set<Element.ID>
+    @Binding var editMode: EditMode
+    let ungroupCallback: ((Element.ID) -> ())?
+    @ViewBuilder var label: () -> LabelView
+    
+    public init(data: Binding<[Element]>,
+                selections: Binding<Set<Element.ID>>,
+                editMode: Binding<EditMode>,
+                ungroupCallback: ((Element.ID) -> ())? = nil,
+                label: @escaping () -> LabelView) {
+        self._data = data
+        self._selections = selections
+        self._editMode = editMode
+        self.ungroupCallback = ungroupCallback
+        self.label = label
+    }
+    
+    var selectedGroupId: Element.ID? {
+        // Helper only returns successful result if all selections are in a group
+        data.containsValidUngroup(from: selections).parentId
+    }
+    
+    // Selections contian one item which is a group
+    var containsValidUngroup: Bool {
+        self.selectedGroupId != nil
+    }
+    
+    public var body: some View {
+        Button {
+            if let selectedGroupId = self.selectedGroupId {
+                // Run the passed-in callback
+                self.ungroupCallback?(selectedGroupId)
+
+                // Update data list
+                self.data = self.data.ungroup(selectedGroupId: selectedGroupId)
+            }
+            
+            self.selections = .init()
+            self.editMode = .inactive
+        } label: {
+            label()
+        }
+        .disabled(!containsValidUngroup)
+    }
+}
+
